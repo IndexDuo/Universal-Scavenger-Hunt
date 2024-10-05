f104e8891a0381434dde2835e3b3b06b6757e163
martezdaniel <155940648+martezdaniel@users.noreply.github.com>
2024-10-05 03:58:39 -0400

    Update HuntList.js
    Ten scavenger hunts have been added to a const variable that is exported and available to use globally

--- components/HuntList.js
+++ components/HuntList.js
@@ -1,8 +1,55 @@
 // components/HuntList.js
 const HUNT_DATA = [
-    { id: "1", title: "Hunt #1: Find the Entrance Sign", description: "Find the main entrance sign and take a picture with it." },
-    { id: "2", title: "Hunt #2: Ride the Jurassic Park Ride", description: "Experience the Jurassic Park ride and capture the moment." },
-    { id: "3", title: "Hunt #3: Take a Picture with a Mascot", description: "Find a park mascot and take a picture with them." },
+    { 
+        id: "1", 
+        title: "Hunt #1: Find the Entrance Sign", 
+        description: `Find Universal Studios' main entrance sign and take a picture with it.` 
+    },
+    { 
+        id: "2", 
+        title: "Hunt #2: Berlin Wall", 
+        description: `There's a piece of the Berlin Wall somewhere in the park. It's located behind a restaurant surrounded by music. Find it and take a photo.` 
+    },
+    { 
+        id: "3", 
+        title: "Hunt #3: Take a Picture with a Mascot", 
+        description: `Find a park mascot and take a picture with them.` 
+    },
+    {
+        id: "4",
+        title: "Hunt #4: Gringotts Dragon",
+        description: `Take a photo of the dragon on the top of Gringotts Bank, while it breathes fire.`
+    },
+    {
+        id: "5",
+        title: "Hunt #5: Norweigan Flag spotting",
+        description: `There's a flag of Norway somewhere in the park. Find it and take a photo.`
+    },
+    {
+        id: "6",
+        title: "Hunt #6: Palace Theater Arcade",
+        description: `Win a prize from the claw machine at the Palace Theater Arcade and take a photo of it.`
+    },
+    {
+        id: "7",
+        title: "Hunt #7: Transformers - Press the Button",
+        description: `While waiting in line for the Transformers ride, there's a button on one of the walls. Press it and take a photo of what the lights do.`
+    },
+    {
+        id: "8",
+        title: "Hunt #8: Stop The Ride!!",
+        description: `In Krustyland, clips from episodes of The Simpsons are shown on TVs. Take a photo of the clip where Bart has a rough experience on a roller coaster.`
+    },
+    {
+        id: "9",
+        title: "Hunt #9: Banana Trees",
+        description: `It's no secret that the Minions love bananas. There's actually banana trees planted in the park because of them. Find them and take a photo.`
+    },
+    {
+        id: "10",
+        title: "Hunt #10: See the Animals",
+        description: `Go see the animals at the Animal Actors on Location show and take a photo of your favorite one. It's worth seeing.`
+    }
 ];
 
 export default HUNT_DATA;
