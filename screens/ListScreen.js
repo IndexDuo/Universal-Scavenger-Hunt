1036311ba6781946ac93152edd4b6f388f6db0c4
martezdaniel <155940648+martezdaniel@users.noreply.github.com>
2024-10-06 04:22:47 +0000

    Oct 6, 2024, 12:22 AM

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,65 +1,21 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  Text,
-  TouchableOpacity,
-} from "react-native";
-import { Card } from "react-native-paper";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import huntsData from "../data/huntsData.json";
-
 function ListScreen({ navigation }) {
-  const [hunts, setHunts] = useState([]);
-  const [completedHunts, setCompletedHunts] = useState({});
-
-  useEffect(() => {
-    setHunts(huntsData);
-
-    async function loadCompletedHunts() {
-      try {
-        const storedStatus = await AsyncStorage.getItem("completedHunts");
-        if (storedStatus) {
-          setCompletedHunts(JSON.parse(storedStatus));
-        }
-      } catch (e) {
-        console.error("Failed to load completed hunts", e);
-      }
-    }
-    loadCompletedHunts();
-  }, []);
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => navigation.navigate("HuntInfoScreen", { huntId: item.id })}
+    >
+      <Card style={styles.card}>
+        <Text>{item.title}</Text>
+      </Card>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={hunts}
+        data={huntsData}
+        renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate("HuntInfoScreen", { huntId: item.id })
-            }
-          >
-            <Card style={styles.card}>
-              <Text>{item.title}</Text>
-            </Card>
-          </TouchableOpacity>
-        )}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  card: {
-    marginVertical: 10,
-    padding: 10,
-  },
-});
-
-export default ListScreen;
